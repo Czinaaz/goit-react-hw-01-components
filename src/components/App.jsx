399165476791c0655a00d450e20e1018e6440610
Ivan Rudenko <rudenko.ivan81@gmail.com>
2023-11-13 15:17:17 +0100

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import Profile from './profile/Profile';
-import Statistics from './statistics/Statistics';
-import FriendList from './friendList/FriendList';
-import TransactionHistory from './transaction/TransactionHistory';
-
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../JSON/user.json';
 import data from '../JSON/data.json';
 import friends from '../JSON/friends.json';
